b3fe4dc96311319c25bb375bd576ffa0238f749f
Otávio Morais <otavio.n.morais@hotmail.com>
2021-02-08 22:40:08 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import './animations/classes.css'
+import { useState } from 'react';
 
 function App() {
+  const [ativarClasse, setAtivarClasse] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p onClick={() => setAtivarClasse(!ativarClasse)}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div style={{height: 300, display: 'flex'}}>
         <a
-          className="App-link"
+          className={`${ativarClasse ? 'cartaJogadaBottom' : ''} cartaBottom`}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        </div>
       </header>
     </div>
   );
